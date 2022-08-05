bcf2236148eaa198afd9f572717508569592d2c6
saareli123 <saarelias1@gmail.com>
2022-08-05 12:48:11 +0300

    all truffic lights

--- src/simulation_V1.0.1.py
+++ src/simulation_V1.0.1.py
@@ -12,16 +12,19 @@ sim.create_signals(G)
 
 # TODO missing leaf nodes on the right of all rows 1-4.. need another node 17
 sim.create_gen({
-    'vehicle_rate': 200,
+    'vehicle_rate': 500,
     'vehicles': [
         [100, {'path': G.getPath("V_1_6_U", "V_1_2_U")}],
         [50, {'path': G.getPath("V_0_4_D", "V_2_4_U")}],
         [90, {'path': G.getPath("V_1_0_D", "V_1_7_D")}],
         [30, {'path': G.getPath("V_2_1_U", "V_0_6_U")}],
-        [80, {'path': G.getPath("V_0_0_D", "V_1_5_D")}],
-        [80, {'path': G.getPath("V_3_0_U", "V_3_17_U")}],
-        [80, {'path': G.getPath("V_1_17_U", "V_2_0_D")}],
-        [80, {'path': G.getPath("V_1_0_D", "V_3_16_D")}],
+        [95, {'path': G.getPath("V_0_0_D", "V_1_5_D")}],
+        [100, {'path': G.getPath("V_3_0_U", "V_3_17_U")}],
+        [75, {'path': G.getPath("V_1_17_U", "V_2_0_D")}],
+        [35, {'path': G.getPath("V_1_0_D", "V_3_16_D")}],
+        [87, {'path': G.getPath("V_5_15_U", "V_0_11_D")}],
+        [100, {'path': G.getPath("V_0_9_D", "V_5_8_U")}],
+        [120, {'path': G.getPath("V_4_0_D", "V_0_10_D")}],
         # [45, {'path': G.getPath("V_0_1_U", "V_1_0_U")}],
     ]
 })
