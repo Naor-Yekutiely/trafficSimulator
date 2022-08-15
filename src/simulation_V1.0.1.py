c295e49092832c7c90bc0265aad005b7d53e7da2
naor.yekutiely@gamil.com <naor.yekutiely@gmail.com>
2022-08-15 20:50:51 +0300

    Added simulation config file

--- src/simulation_V1.0.1.py
+++ src/simulation_V1.0.1.py
@@ -1,4 +1,3 @@
-import numpy as np
 from trafficSimulator import *
 
 
@@ -10,55 +9,18 @@ sim.create_nodes(G)
 sim.create_signals(G)
 sim.setGraph(G)
 
-# TODO missing leaf nodes on the right of all rows 1-4.. need another node 17
+f = open(f"{os.getcwd()}/src/trafficSimulator/Simulation_Config.json")
+simulationConfig = json.load(f)
+vehiclesGen = []
+for path in simulationConfig["phats"]:
+    vehiclesGen.append([int(path['rate']), {'path': G.getPath(path["source"], path["target"]),
+                                            'edgesPath': G.indexPathToEdgesPath(G.getPath(path["source"], path["target"])),
+                                            'source': path["source"], 'target': path["target"]}])
 sim.create_gen({
     'vehicle_rate': 500,
-    'vehicles': [
-        [100, {'path': G.getPath("V_1_6_U", "V_1_2_U"),
-               'edgesPath': G.indexPathToEdgesPath(G.getPath("V_1_6_U", "V_1_2_U")),
-               'source': 'V_1_6_U', 'target': 'V_1_2_U'}],
-        [50, {'path': G.getPath("V_0_4_D", "V_2_4_U"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_0_4_D", "V_2_4_U")),
-              'source': 'V_0_4_D', 'target': 'V_2_4_U'}],
-        [90, {'path': G.getPath("V_1_0_D", "V_1_7_D"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_1_0_D", "V_1_7_D")),
-              'source': 'V_1_0_D', 'target': 'V_1_7_D'}],
-        [30, {'path': G.getPath("V_2_1_U", "V_0_6_U"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_2_1_U", "V_0_6_U")),
-              'source': 'V_2_1_U', 'target': 'V_0_6_U'}],
-        [95, {'path': G.getPath("V_0_0_D", "V_1_5_D"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_0_0_D", "V_1_5_D")),
-              'source': 'V_0_0_D', 'target': 'V_1_5_D'}],
-        [100, {'path': G.getPath("V_3_0_U", "V_3_17_U"),
-               'edgesPath': G.indexPathToEdgesPath(G.getPath("V_3_0_U", "V_3_17_U")),
-               'source': 'V_3_0_U', 'target': 'V_3_17_U'}],
-        [75, {'path': G.getPath("V_1_17_U", "V_2_0_D"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_1_17_U", "V_2_0_D")),
-              'source': 'V_1_17_U', 'target': 'V_2_0_D'}],
-        [35, {'path': G.getPath("V_1_0_D", "V_3_16_D"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_1_0_D", "V_3_16_D")),
-              'source': 'V_1_0_D', 'target': 'V_3_16_D'}],
-        [87, {'path': G.getPath("V_5_15_U", "V_0_11_D"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_5_15_U", "V_0_11_D")),
-              'source': 'V_5_15_U', 'target': 'V_0_11_D'}],
-        [100, {'path': G.getPath("V_0_9_D", "V_5_8_U"),
-               'edgesPath': G.indexPathToEdgesPath(G.getPath("V_0_9_D", "V_5_8_U")),
-               'source': 'V_0_9_D', 'target': 'V_5_8_U'}],
-        [120, {'path': G.getPath("V_4_0_D", "V_0_10_D"),
-               'edgesPath': G.indexPathToEdgesPath(G.getPath("V_4_0_D", "V_0_10_D")),
-               'source': 'V_4_0_D', 'target': 'V_0_10_D'}],
-        [75, {'path': G.getPath("V_1_16_D", "V_5_12_U"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_1_16_D", "V_5_12_U")),
-              'source': 'V_1_16_D', 'target': 'V_5_12_U'}],
-        [35, {'path': G.getPath("V_4_17_D", "V_1_7_U"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_4_17_D", "V_1_7_U")),
-              'source': 'V_4_17_D', 'target': 'V_1_7_U'}],
-        [87, {'path': G.getPath("V_0_2_U", "V_5_8_U"),
-              'edgesPath': G.indexPathToEdgesPath(G.getPath("V_0_2_U", "V_5_8_U")),
-              'source': 'V_0_2_U', 'target': 'V_5_8_U'}],
-        # [45, {'path': G.getPath("V_0_1_U", "V_1_0_U")}],
-    ]
+    'vehicles': vehiclesGen
 })
+
 # Start simulation
 win = Window(sim)
 win.zoom = 1.5
