64fd608073c2150852b0a86fb3765e6f66c80e6a
naor.yekutiely@gamil.com <naor.yekutiely@gmail.com>
2022-10-20 19:02:48 +0300

    fixed comments typos

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import subprocess
 
 # TODO:
-# 1. Remove all unneeded files and fix all comments typos.
 # 2. Add more boards in Grafana to make it look more impressive.
 
 main_window = Tk()
